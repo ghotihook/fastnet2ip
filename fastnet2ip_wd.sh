406861fb59c3afeb6e07b87267352ac93de9cd39
ghotihook <alex@ivila.net>
2025-01-18 00:49:22 +1100

    logging update

--- fastnet2ip_wd.sh
+++ fastnet2ip_wd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-	/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttyUSB0 -u 2002 --log-level ERROR
+	/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttyUSB0 -u 2002 --log-level INFO
 	sleep 5
 done
