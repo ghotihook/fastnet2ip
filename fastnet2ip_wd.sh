b2b048defa8217badbc6fed9fba950f059e1f4ab
ghotihook <alex@ivila.net>
2025-01-21 05:39:12 +1100

    minor change to wd script to refelct new args

--- fastnet2ip_wd.sh
+++ fastnet2ip_wd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 while true; do
-	/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttySTM3 -u 2002 --log-level ERROR
+	/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttySTM3 -udp-port 2002 --log-level ERROR
+	#/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttyUSB0 -udp-port 2002 --log-level ERROR
 	sleep 5
 done
