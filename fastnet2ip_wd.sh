4a721be19b6c5e71ff15225ba375575bd5e36cee
ghotihook <alex@ivila.net>
2025-01-21 06:08:23 +1100

    minor change to wd

--- fastnet2ip_wd.sh
+++ fastnet2ip_wd.sh
@@ -3,7 +3,7 @@
 while true; do
 	/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttySTM3 --udp-port 2002 --log-level ERROR
 	#/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttyUSB0 --udp-port 2002 --log-level ERROR
-	#/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttyUSB0 --udp-port 2002 --log-level ERROR
+	#/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --serial /dev/ttyAMA0 --udp-port 2002 --log-level ERROR
 	#/home/alex060/python_environment/bin/python3 /home/alex060/fastnet2ip/fastnet2ip.py --file example1_fastnet_data.txt --udp-port 2002 --log-level ERROR --live-data
 	sleep 5
 done
